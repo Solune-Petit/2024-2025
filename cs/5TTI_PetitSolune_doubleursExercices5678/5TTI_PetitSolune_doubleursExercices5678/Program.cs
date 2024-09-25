2fbc3c17974067751b0a2f01ccc74394f2ae8b3a
Solune-Petit <210225@site.asty-moulin.be>
2024-09-25 14:19:18 +0200

    14h20_25-09-24

---
+++ cs/5TTI_PetitSolune_doubleursExercices5678/5TTI_PetitSolune_doubleursExercices5678/Program.cs
@@ -0,0 +1,28 @@
+﻿using _5T24_PetitSolune_enigma;
+using System.Drawing;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+
+namespace _5TTI_PetitSolune_doubleursExercices5678
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ColorChanger couleur = new ColorChanger();
+            fonctions mesOutils = new fonctions();
+
+
+
+            couleur.blue();
+            Console.WriteLine("Bienvenue dans mon projet !!!");
+            couleur.green();
+            Console.WriteLine("que voulez-vous faire ?");
+            couleur.red();
+            Console.WriteLine(  "1 : savoir si un entier naturel que vous proposez est cubique ou non" +
+                                "\n2 : déterminer tous les chiffres qui divisent un nombre (valeur max : 10000)\n" +
+                                "3 : trouver un nombre naturel entre de valeurs\n" +
+                                "4 : générer les premiers nombres premiers");
+        }
+    }
+}
