40df60eed3c7e2f5ede0f2ad00bfba6d29ae5efe
Solune-Petit <210225@site.asty-moulin.be>
2024-10-08 13:52:32 +0200

    13h52_08-10-24

---
+++ cs/5TTI_PetitSolune_doubleursExercice9/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _5TTI_PetitSolune_doubleursExercice9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
