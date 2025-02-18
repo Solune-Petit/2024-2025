935da2e0eb6a46cf1e3d19b7bc689eb4c2cc86af
Solune-Petit <210225@site.asty-moulin.be>
2025-02-18 13:19:00 +0100

    2025-02-18_13h18

---
+++ projets_fin_annee/CSharp/5T24_PetitSolune_BlackJack/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _5T24_PetitSolune_BlackJack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Black Jack!");
+        }
+    }
+}
